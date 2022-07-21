3bdf3287280242fa97c576193d0777d2b2a0db73
Christian Rivas <crivas-2020025@kinal.edu.gt>
2022-07-20 23:59:49 -0600

    Detalle de la Noticia

--- src/app/components/noticia-detalle/noticia-detalle.component.ts
+++ src/app/components/noticia-detalle/noticia-detalle.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { ActivatedRoute } from '@angular/router';
+//NOTICIAS
+import { Noticias } from 'src/app/models/noticias.model';
+import { NoticiasService } from 'src/app/services/noticias.service';
+
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-noticia-detalle',
   templateUrl: './noticia-detalle.component.html',
-  styleUrls: ['./noticia-detalle.component.scss']
+  styleUrls: ['./noticia-detalle.component.scss'],
+  providers: [ UsuarioService, NoticiasService ]
 })
 export class NoticiaDetalleComponent implements OnInit {
 
-  constructor() { }
+  public token;
+  public idNoticia;
+
+  //Noticias
+  public noticiasModelGetId: Noticias;
+
+  constructor(public _usuarioService: UsuarioService, public _noticiasService: NoticiasService, public _activatedRoute: ActivatedRoute) {
+    this.noticiasModelGetId = new Noticias('','', '','', '');
+    this.token = this._usuarioService.obtenerToken();
+   }
 
   ngOnInit(): void {
+    this._activatedRoute.paramMap.subscribe((dataRuta)=>{
+      this.getNoticiasId(dataRuta.get('idNoticia'));
+
+      this.idNoticia = dataRuta.get('idNoticia');
+    });
+
+  }
+
+  getNoticiasId(idNoticia){
+    this._noticiasService.obtenerNoticiaId(idNoticia, this._usuarioService.obtenerToken()).subscribe(
+      (response) => {
+        this.noticiasModelGetId = response.noticias;
+        console.log(response);
+        console.log(this.noticiasModelGetId);
+      },
+      (error)=>{
+        console.log(<any>error)
+      }
+    )
   }
 
 }
